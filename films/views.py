1dc85cf79f7720b480745d826a7a8a3a7838e1bf
bitfez <kibrisli7@gmail.com>
2023-12-24 09:33:26 +0000

    imported data & added pagination

--- films/views.py
+++ films/views.py
@@ -1,12 +1,45 @@
 from django.shortcuts import render, get_object_or_404
 from django.db.models import Avg
+import os, json
+from datetime import timedelta
+from django.http import HttpResponse
+from django.core.paginator import Paginator
 
 from . models import Film, Rating
+module_dir = os.path.dirname(__file__)  # get current directory
 # Create your views here.
+def add_films(request):
+    file_path = os.path.join(module_dir, 'imdb_top_films.json')
+    with open(file_path, "r", encoding="utf-8") as f:
+        data = json.load(f)
+    no_added = 0
+    for film in data:
+        dur = film["Runtime"]
+        f_dur = timedelta(minutes=int(dur[0:-4]))
+        obj = Film.objects.update_or_create(
+            title = film["Series_Title"],
+            released = f"{film['Released_Year']}-01-01",
+            certificate = film["Certificate"],
+            duration = f_dur,
+            genre = film["Genre"],
+            director = film["Director"],
+            star1 = film["Star1"],
+            star2 = film["Star2"],
+            star3 = film["Star3"],
+            star4 = film["Star4"],
+            overview = film["Overview"],
+            poster = film["Poster_Link"]
+        )
+        no_added += 1
+    msg = f"Added {no_added} films to the database."
+    return HttpResponse(msg, content_type="text/plain")
+
 def homepage(request):
     films = Film.objects.all()
-    
-    context = {"films":films}
+    paginator = Paginator(films, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {"films":films, "page_obj":page_obj}
     return render(request, "films/homepage.html", context)
 
 def film_detail(request, pk):
@@ -15,6 +48,6 @@ def film_detail(request, pk):
     hours = total_seconds//3600
     minutes = (total_seconds % 3600 )//60
     duration = f"{hours}h {minutes}m"
-
-    context = {"film":film, "dura":duration}
+    genres = film.genre.split(",")
+    context = {"film":film, "dura":duration, "genres":genres}
     return render(request, "films/f_detail.html", context)
