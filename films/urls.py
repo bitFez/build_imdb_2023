9db5cecff8577b945fd557167c82ff277fa19d94
bitfez <kibrisli7@gmail.com>
2023-12-27 11:19:02 +0000

    tested running adding random reviews

--- films/urls.py
+++ films/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 
-from . views import homepage, film_detail, add_films
+from . views import homepage, film_detail, add_films,add_review
 
 app_name = "films"
 
 urlpatterns = [
     path("", homepage, name="home"),
     path("film/<int:pk>/", film_detail, name="f_detail"),
-    path("add_films", add_films, name="add_films")
+    path("add_films", add_films, name="add_films"),
+    path("add_review", add_review, name="add_review"),
 ]
--- films/views.py
+++ films/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.db.models import Avg
 import os, json
 from datetime import timedelta
 from django.http import HttpResponse
 from django.core.paginator import Paginator
+from random import randint
+from django.contrib.auth.models import User
 
 from . models import Film, Rating
 module_dir = os.path.dirname(__file__)  # get current directory
@@ -34,6 +36,14 @@ def add_films(request):
     msg = f"Added {no_added} films to the database."
     return HttpResponse(msg, content_type="text/plain")
 
+def add_reviews(given_film, given_user, given_rating):
+    obj = Rating.objects.update_or_create(
+        film = get_object_or_404(Film, pk=given_film),
+        user = get_object_or_404(User, pk=given_user),
+        rating = given_rating
+    )
+    
+
 def homepage(request):
     films = Film.objects.all()
     paginator = Paginator(films, 10)
@@ -51,3 +61,10 @@ def film_detail(request, pk):
     genres = film.genre.split(",")
     context = {"film":film, "dura":duration, "genres":genres}
     return render(request, "films/f_detail.html", context)
+
+def add_review(request):
+    for i in range(1,5):
+        filmID = randint(1,680)
+        film_rating = randint(1,10)
+        add_reviews(filmID, i, film_rating)
+    return redirect("films:home")
